98ab212bcdf945d5bbfa97e5326504cb8efd9609
RSaru <rs1447@student.aru.ac.uk>
2022-10-20 10:12:15 +0100

    adds in code to calculate area and circum+ methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 int[] numbers = new int[5];
 
 for (int i =0; i<5; i++)
@@ -11,5 +11,31 @@ Array.Sort(numbers);
 
 Console.WriteLine("The Highest Number is " + numbers[4]);
 Console.WriteLine("The Lowest Number is " + numbers[0]);
+*/
 
-Console.ReadLine();
+int radius;
+Console.WriteLine("Welcome to my program");
+
+double areaCalc()
+{
+    Console.WriteLine("Enter a radius");
+    radius = Convert.ToInt32(Console.ReadLine());
+    double answer = Math.PI * (radius*radius);
+    return answer;
+}
+
+int circum;
+
+double cirCalc()
+{
+    Console.WriteLine("Enter a radius");
+    circum = Convert.ToInt32(Console.ReadLine());
+    double answer = 2* Math.PI * radius;
+    return answer;
+}
+Console.ReadKey();
+
+Console.WriteLine("area is " + areaCalc());
+//basically just prints answer but makes it more user friendly
+Console.WriteLine("The Circumphrence is " + cirCalc());
+Console.ReadKey();
