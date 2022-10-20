91e47a1480f492bb29e4c813b748d055f2ce5c2e
RSaru <rs1447@student.aru.ac.uk>
2022-10-20 09:27:57 +0100

    Added a number sorting array

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] numbers = new int[5];
+
+for (int i =0; i<5; i++)
+{
+    Console.WriteLine("Enter a number: " + i);
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Array.Sort(numbers);
+
+Console.WriteLine("The Highest Number is " + numbers[4]);
+Console.WriteLine("The Lowest Number is " + numbers[0]);
+
+Console.ReadLine();
