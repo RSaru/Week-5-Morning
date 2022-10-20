583e12de19cdfab2f810d6f536b9bde6c1d52398
RSaru <rs1447@student.aru.ac.uk>
2022-10-20 10:56:13 +0100

    updated both methods to be user friendly

--- Program.cs
+++ Program.cs
@@ -16,26 +16,28 @@ Console.WriteLine("The Lowest Number is " + numbers[0]);
 int radius;
 Console.WriteLine("Welcome to my program");
 
-double areaCalc()
+double areaCalc(int radius)
 {
-    Console.WriteLine("Enter a radius");
-    radius = Convert.ToInt32(Console.ReadLine());
     double answer = Math.PI * (radius*radius);
     return answer;
 }
 
 int circum;
 
-double cirCalc()
+double cirCalc(int radius)
 {
-    Console.WriteLine("Enter a radius");
-    circum = Convert.ToInt32(Console.ReadLine());
     double answer = 2* Math.PI * radius;
     return answer;
 }
 Console.ReadKey();
-
-Console.WriteLine("area is " + areaCalc());
+Console.WriteLine("Enter a radius");
+radius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("area is " + areaCalc(radius));
 //basically just prints answer but makes it more user friendly
-Console.WriteLine("The Circumphrence is " + cirCalc());
+
+
+
+Console.WriteLine("Enter a radius");
+radius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The Circumphrence is " + cirCalc(radius));
 Console.ReadKey();
